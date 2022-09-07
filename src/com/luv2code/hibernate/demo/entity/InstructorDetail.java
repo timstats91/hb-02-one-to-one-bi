0b82b139beb2073b466e06db0ddca3033ae301e7
timstats91 <timstats91@gmail.com>
2022-09-07 12:24:29 -0500

    initial commit

--- src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
+++ src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
@@ -1,10 +1,12 @@
 package com.luv2code.hibernate.demo.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 //annotate the class as an entity and map to db table
@@ -25,6 +27,11 @@ public class InstructorDetail {
 	
 	@Column(name="hobby")
 	private String hobby;
+	
+	// add new field for instructor
+	
+	@OneToOne(mappedBy="instructorDetail", cascade=CascadeType.ALL)
+	private Instructor instructor;
 	
 	// create constructors
 	
@@ -62,6 +69,14 @@ public class InstructorDetail {
 
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
 	}
 
 	
